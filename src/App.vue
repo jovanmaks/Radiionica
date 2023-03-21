2ac7df233ffe95dfb8c628f580c316b679361bea
jovanmaks <jovanmaks92@gmail.com>
2023-03-21 09:13:20 +0100

    rad na povezivanju baze

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,43 @@
   </ion-app>
 </template>
 
-<script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+<script stup lang="ts">
+import { IonApp, IonRouterOutlet,useIonRouter } from '@ionic/vue';
 import MenyPage from './views/MenyPage.vue';
+import { defineComponent } from 'vue'
+
+
+import { store } from './store'
+import { supabase } from './supabase'
+
+console.log('OVDJE-start');
+console.log(supabase.auth.admin );
+console.log(supabase.auth.getUser() );
+console.log('OVDJE-end');
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet
+  }
+});
+
+// export default defineComponent({
+//     name: 'App',
+//     components: {
+//       IonApp,
+//       IonRouterOutlet,
+//     },
+//     setup() {
+//       const router = useIonRouter()
+//       store.user = supabase.auth.user ?? {}
+//       supabase.auth.onAuthStateChange((_, session) => {
+//         store.user = session?.user ?? {}
+//         if (session?.user) {
+//           router.replace('/account')
+//         }
+//       })
+//     },
+//   })
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,4 +1,4 @@
-
+<!-- 
 <template>
 
   <base-layout page-title="Home" page-default-back-link="/tabs" >
@@ -11,4 +11,69 @@
     import { IonList, IonTitle,IonItem, IonTabBar,IonMenu, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
     import { ellipse, square, triangle,star } from 'ionicons/icons';
 
+  </script> -->
+  <template>
+    <ion-page>
+      <ion-header :translucent="true">
+        <ion-toolbar>
+          <ion-title>Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+      <ion-content :fullscreen="true">
+        <ion-header collapse="condense">
+          <ion-toolbar>
+            <ion-title size="large">Blank</ion-title>
+          </ion-toolbar>
+        </ion-header>
+      
+        <div id="container">
+          <strong>Ready to create an app?</strong>
+          <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        </div>
+      </ion-content>
+    </ion-page>
+  </template>
+  
+  <script lang="ts">
+  import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'HomePage',
+    components: {
+      IonContent,
+      IonHeader,
+      IonPage,
+      IonTitle,
+      IonToolbar
+    }
+  });
   </script>
+  
+  <style scoped>
+  #container {
+    text-align: center;
+    
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  #container strong {
+    font-size: 20px;
+    line-height: 26px;
+  }
+  #container p {
+    font-size: 16px;
+    line-height: 22px;
+    
+    color: #8c8c8c;
+    
+    margin: 0;
+  }
+  #container a {
+    text-decoration: none;
+  }
+  </style>
+  
