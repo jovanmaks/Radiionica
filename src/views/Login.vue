5c5d806d7cd128756eb5bd5991b577653c9ea751
jovanmaks <jovanmaks92@gmail.com>
2023-03-24 10:12:05 +0100

    logovanje radi

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,8 @@
           <p>Sign in via magic link with your email below</p>
         </div>
         <ion-list :inset="true">
-          <form @submit.prevent="handleLogin">
+          <form @submit.prevent="login">
+
             <ion-item>
               <ion-label position="stacked">Email</ion-label>
               <ion-input
@@ -22,6 +23,18 @@
                 type="email"
               ></ion-input>
             </ion-item>
+            
+            
+            <ion-item>
+              <ion-label position="stacked">Password</ion-label>
+              <ion-input
+                v-model="password"
+                type="password"
+                name="password"
+                autocomplete="name"
+              ></ion-input>
+            </ion-item>
+
             <div class="ion-text-center">
               <ion-button type="submit" fill="clear">Login</ion-button>
             </div>
@@ -50,6 +63,7 @@
     loadingController,
   } from '@ionic/vue';
   import { defineComponent, ref } from 'vue';
+  import { useRouter } from "vue-router";
   export default defineComponent({
     name: 'LoginPage',
     components: {
@@ -65,24 +79,44 @@
       IonButton,
     },
     setup() {
+      const router = useRouter();
       const email = ref('');
-      const handleLogin = async () => {
-        const loader = await loadingController.create({});
-        const toast = await toastController.create({ duration: 5000 });
-        try {
-          await loader.present();
-          const { error } = await supabase.auth.signInWithPassword({ email: email.value,password: 'password' });
-          if (error) throw error;
-          toast.message = 'Check your email for the login link!';
-          await toast.present();
-        } catch (error: any) {
-          toast.message = error.error_description || error.message;
-          await toast.present();
-        } finally {
-          await loader.dismiss();
+      const password = ref('');
+      const errorMsg = ref('');
+
+      const login = async () => {
+        try{
+          const {error} = await supabase.auth.signInWithPassword({
+            email: email.value,
+            password: password.value,
+          });
+          if(error) throw error; 
+          router.push({name: "Account"});
+        }catch(error){
+          // errorMsg.value =  
+
         }
-      };
-      return { handleLogin, email };
+      }
+
+
+      // const handleLogin = async () => {
+      //   const loader = await loadingController.create({});
+      //   const toast = await toastController.create({ duration: 5000 });
+      //   try {
+      //     await loader.present();
+      //     const { error } = await supabase.auth.signInWithPassword({ email: email.value,password: password.value, });
+      //     if (error) throw error;
+      //     toast.message = 'Check your email for the login link!';
+      //     await toast.present();
+      //   } catch (error: any) {
+      //     toast.message = error.error_description || error.message;
+      //     await toast.present();
+      //   } finally {
+      //     await loader.dismiss();
+      //   }
+      // };
+
+      return {email, password, errorMsg, login};
     },
   });
   </script>
