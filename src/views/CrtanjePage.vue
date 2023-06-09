7b23d26d25339b8cf4145526a231d068c381f08c
jovanmaks <jovanmaks92@gmail.com>
2023-06-09 19:00:37 +0200

    confirm na modalu sada radi

--- src/views/CrtanjePage.vue
+++ src/views/CrtanjePage.vue
@@ -38,11 +38,22 @@
         <ion-label position="stacked">Unesi postotak</ion-label>
         <ion-input ref="valueInput" type="number" placeholder="Postotak"></ion-input>
       </ion-item>
+
+      <ion-item v-for="(section, index) in modalSections" :key="index">
+      <ion-segment :value="section.done ? 'done' : 'notDone'" @ionChange="updateStatus(index, $event.detail.value)">
+        <ion-segment-button value="done">
+          <ion-label>Done</ion-label>
+        </ion-segment-button>
+        <ion-segment-button value="notDone">
+          <ion-label>Not Done</ion-label>
+        </ion-segment-button>
+      </ion-segment>
+    </ion-item>
+
       </ion-content>
+
     </ion-modal>
   </ion-content>
-
-
 
     </template>
   </base-layout>
@@ -66,13 +77,16 @@
     IonFabButton,
     IonContent,
     IonToolbar,
-    IonInput,
+    // IonInput,
     IonPage,
+    IonSegment, 
+    IonSegmentButton,
     IonRouterOutlet } from '@ionic/vue';
     import { OverlayEventDetail } from '@ionic/core/components';
     import { onMounted, ref, defineComponent } from 'vue';
     import { Chart, registerables } from 'chart.js';
     import { useRouter, RouterLink } from "vue-router";
+    import { supabase } from '@/supabase';
     
   
   Chart.register(...registerables);
@@ -88,11 +102,13 @@
       IonToolbar,
       IonTitle,
       IonItem,
-      IonInput,
+      // IonInput,
       IonLabel,
       IonIcon,
       IonFab,
       IonFabButton,
+      // IonSegment, 
+      // IonSegmentButton,
     },
 
     data() {
@@ -113,66 +129,93 @@
       cancel() {
         this.$refs.modal.$el.dismiss(null, 'cancel');
       },
+      
+   
       confirm() {
-        const name = this.$refs.nameInput.$el.value;
-        const value = this.$refs.valueInput.$el.value;
-        this.sections.push({ name, value });
-        this.$refs.modal.$el.dismiss(name, 'confirm');
-      },
-      // existing methods
+        console.log(this.$refs);
+  const name = this.$refs.nameInput.value;
+  const value = this.$refs.valueInput.value;
+  const data = {name: name, value: value};
+  this.$refs.modal.$el.dismiss(data, 'confirm');
+},
+
+
+
+onWillDismiss: function(ev) {
+  if (ev.detail.role === 'confirm') {
+    this.message = 'Hello, ' + ev.detail.data.name + ', your value is ' + ev.detail.data.value + '!';
+  }
+},
     },
 
 
     setup() {
 
 
-   
+      const modalSections = ref([]);
+      const selectedProjectName = localStorage.getItem('selectedProject');
+
 
 
       const chart = ref(null);
   
-      onMounted(() => {
-        const ctx = chart.value.getContext('2d');
-        new Chart(ctx, {
-          type: 'doughnut',
-          data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            cutout: '50%',
-          }
-        });
-      });
+    
   
+
+      const loadSelectedProject = async () => {
+      const { data: fetchedData, error } = await supabase
+        .from('Projekti')
+        .select('*')
+        .eq('ime_projekta', selectedProjectName);
+
+      if (error) {
+        console.error('Error loading project:', error);
+        return;
+      }
+
+      return fetchedData[0];
+    };
+
+
+
+      onMounted(async () => {
+    const selectedProject = await loadSelectedProject();
+    modalSections.value = Array(selectedProject.broj_objekata)
+        .fill()
+        .map((_, index) => ({ 
+          name: `Object ${index + 1}`, 
+          value: 1, // Replace with real value
+          done: false 
+        }));
+
+    const ctx = chart.value.getContext('2d');
+    new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: Array(selectedProject.broj_objekata).fill().map((_, i) => `Object ${i + 1}`),
+        datasets: [{
+          label: '# of Votes',
+          data: Array(selectedProject.broj_objekata).fill(1), // Replace with real data
+          backgroundColor: 'rgba(128, 128, 128, 1)', // Gray color
+          borderColor: 'rgba(0, 0, 0, 1)', // Gray color
+          borderWidth: 1
+        }]
+      },
+      options: {
+        cutout: '50%',
+      }
+    });
+});
   
   return {
     chart,
-    add
+    add, modalSections
     };
 
     }
+    
   };
+
   </script>
 
 
