823a509e4c74761abf23a4285ecdd4778754d791
jovanmaks <jovanmaks92@gmail.com>
2023-06-22 22:06:50 +0200

    Sredjeno editovanje biljeski

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -19,12 +19,45 @@
           <ion-button class="alert-button" fill="clear" @click="setOpen(true, note)">
             <ion-action-sheet :is-open="isOpen" header="Приоритети" :buttons="actionSheetButtonsRef"
               @didDismiss="setOpen(false)"></ion-action-sheet>
-
             <ion-icon :icon="alertCircle"></ion-icon>
+          </ion-button>
+
+          <ion-button class="edit-button" fill="clear" @click="setOpenEdit(true, note)">
+            <!-- <ion-alert :is-open="isOpenEdit" header="Уреди" :buttons="alertButtonsEditRef"
+              @didDismiss="setOpenEdit(false)"></ion-alert> -->
+            <ion-icon :icon="documentText"></ion-icon>
           </ion-button>
 
         </div>
       </ion-card>
+
+
+
+      <ion-modal :is-open="isOpenEdit" css-class="my-custom-class" @didDismiss="setOpenEdit(false)">
+        <ion-header>
+          <ion-toolbar>
+            <ion-buttons slot="start">
+              <ion-button @click="setOpenEdit(false)">Откажи</ion-button>
+            </ion-buttons>
+            <!-- <ion-title>Notifikacija</ion-title> -->
+            <ion-buttons slot="end">
+              <ion-button :strong="true" @click="confirmChanges">Потврди</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <Modal :data="data"></Modal>
+        <ion-content class="ion-padding">
+          <div class="input-button-container">
+            <ion-item style="flex-grow: 1">
+              <ion-input v-model="newNote" placeholder="Enter Note"></ion-input>
+            </ion-item>
+
+            <!-- Add Note Button -->
+            <!-- <ion-button :strong="true" @click="addBiljeska" style="margin-left: 5px;">Add Note</ion-button> -->
+          </div>
+        </ion-content>
+      </ion-modal>
+
 
       <div v-if="!isDataLoaded">Loading...</div>
 
@@ -164,8 +197,9 @@
 <script lang="js">
 
 import {
-  IonActionSheet, IonIcon, IonFab, IonFabButton, IonFabList, IonCard, IonCardHeader,
-  IonCardSubtitle, IonCardContent, IonRefresher, IonRefresherContent,
+  IonActionSheet, IonIcon, IonFab, IonFabButton, IonFabList, IonCard, IonCardHeader, IonButton, IonButtons, IonItem,
+  IonCardSubtitle, IonCardContent, IonRefresher, IonRefresherContent, IonInput, IonModal, IonHeader, IonToolbar, IonContent,
+  // IonAlert,
 } from '@ionic/vue';
 
 import { onMounted, ref } from 'vue';
@@ -175,6 +209,7 @@ import {
   ellipse, alertCircle, square, triangle, star, library, hammer, server, easel,
   add, archive, build, construct, folder, cash, brush, desktop, layers, cut,
   extensionPuzzle, colorFill, create, cart, camera, car, checkmarkDone, list, funnel,
+  documentText,
 } from 'ionicons/icons';
 
 
@@ -182,8 +217,9 @@ import {
 export default {
   name: 'HomePage',
   components: {
-    IonActionSheet, IonFab, IonFabButton, IonFabList, IonIcon, IonCard, IonCardHeader,
-    IonCardSubtitle, IonCardContent, IonRefresher, IonRefresherContent,
+    IonActionSheet, IonFab, IonFabButton, IonFabList, IonIcon, IonCard, IonCardHeader, IonButton, IonButtons, IonItem,
+  IonCardSubtitle, IonCardContent, IonRefresher, IonRefresherContent, IonInput,IonModal, IonHeader, IonToolbar,IonContent ,
+    // IonAlert,
   },
 
   setup() {
@@ -192,10 +228,17 @@ export default {
     const isDataLoaded = ref(false);
     const isOpen = ref(false);
     const isOpenFilter = ref(false);
+    const isOpenEdit = ref(false);
     const selectedColor = ref(''); // default color can be empty or any valid ionic color
 
     const actionSheetButtonsRef = ref([]);
     const actionSheetButtonsFilterRef = ref([]);
+    // const alertButtonsEditRef = ref([]);
+
+    const selectedNote = ref(null); // This will store the currently selected note
+    const newNote = ref(''); // This will store the updated text of the note
+
+
 
     const changeColor = (color) => {
       selectedColor.value = color;
@@ -251,6 +294,34 @@ export default {
       isOpenFilter.value = state;
     };
 
+    const setOpenEdit = (state, note = null) => {
+      console.log('edit opened', state, note);
+      if (state) {
+        selectedNote.value = note; // When the modal is opening, set the selected note
+        newNote.value = note.homescreen; // Initialize the new note text to be the same as the current note's text
+        console.log('selected note', selectedNote.value);
+        console.log('new note', newNote.value);
+      } else {
+        selectedNote.value = null; // When the modal is closing, clear the selected note
+      }
+      isOpenEdit.value = state;
+    };
+
+    const confirmChanges = async () => {
+      if (selectedNote.value) { // If a note is selected
+        const { data, error } = await supabase
+          .from('notes')
+          .update({ homescreen: newNote.value }) // Update the kreator field with the new text
+          .eq('id', selectedNote.value.id); // Find the note by its id
+          console.log('selected note', newNote.value);
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(`Note ${selectedNote.value.id} updated successfully`);
+        }
+      }
+      setOpenEdit(false);
+    };
 
     const actionSheetButtons = (note) => [
       {
@@ -334,6 +405,11 @@ export default {
         },
       },
     ];
+
+    const alertButtons = ['OK'];
+
+
+
     const computeCardColor = (note) => {
       if (note.levelOne) return 'danger';
       if (note.levelTwo) return 'warning';
@@ -369,12 +445,14 @@ export default {
     return {
       library, hammer, server, easel, add, archive, alertCircle, build, construct, folder, cash, funnel,
       brush, desktop, layers, cut, colorFill, extensionPuzzle, create, cart, camera, car,
-      checkmarkDone, list, notes, isDataLoaded, doRefresh, archiveNote, isOpen, isOpenFilter,
-      setOpen, setOpenFilter, changeColor, selectedColor, computeCardColor,
-      actionSheetButtons, actionSheetButtonsFilter, actionSheetButtonsRef, actionSheetButtonsFilterRef,
-      sortByPriority,
-      sortById,
-      sortByCreator,
+      checkmarkDone, list, notes, isDataLoaded, doRefresh, archiveNote, isOpen, isOpenFilter, isOpenEdit,
+      setOpen, setOpenFilter, setOpenEdit, changeColor, selectedColor, computeCardColor,
+      actionSheetButtons, actionSheetButtonsFilter,
+      //  alertButtonsEdit,
+      actionSheetButtonsRef, actionSheetButtonsFilterRef,
+      // alertButtonsEditRef,
+      sortByPriority, sortById, sortByCreator, documentText, alertButtons,
+      confirmChanges, newNote,
     }
   },
 };
